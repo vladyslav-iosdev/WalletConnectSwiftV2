4a9f6a05bccd96fa879d5ed2771b31809b345c8c
llbartekll <>
2021-10-01 15:59:15 +0200

    add pairing payload marshaling

--- Sources/WalletConnect/Types/Pairing/ClientSynchronisation/PairingPayloadParams.swift
+++ Sources/WalletConnect/Types/Pairing/ClientSynchronisation/PairingPayloadParams.swift
@@ -11,17 +11,30 @@ extension PairingType.PayloadParams {
     struct Request: Codable, Equatable {
         let method: PairingType.PayloadMethods
         let params: SessionType.ProposeParams
-    }
-}
-
-extension PairingType.PayloadParams.Request {
-    enum Params: Codable, Equatable {
-        init(from decoder: Decoder) throws {
-            fatalError("not implemented")
+        
+        enum CodingKeys: CodingKey {
+            case method
+            case params
         }
         
-        func encode(to encoder: Encoder) throws {
-            fatalError("not implemented")
+        init(method: PairingType.PayloadMethods, params: SessionType.ProposeParams) {
+            self.method = method
+            self.params = params
+        }
+        
+        public init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            method  = try container.decode(PairingType.PayloadMethods.self, forKey: .method)
+            let paramsString  = try container.decode(String.self, forKey: .params)
+            let paramsData = paramsString.data(using: .utf8)!
+            params = try JSONDecoder().decode(SessionType.ProposeParams.self, from: paramsData)
+        }
+        
+        public func encode(to encoder: Encoder) throws {
+            var container = encoder.container(keyedBy: CodingKeys.self)
+            try container.encode(method, forKey: .method)
+            let jsonString = try params.json()
+            try container.encode(jsonString, forKey: .params)
         }
     }
 }
